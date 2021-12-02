d5f06467d861dcf0456534cdf2bf9fe9fa09ca73
Isaque Melo <isaque.melo@mercadolivre.com>
2021-12-02 13:57:16 -0300

    chore: Undo to starting point

--- app.ts
+++
@@ -1,6 +0,0 @@
-export default class PalindromeChecker {
-    static isValid(text: String): boolean {
-        const uncasedText = text.toLowerCase()
-        return uncasedText.split("").reverse().join("") === uncasedText
-    }
-}
